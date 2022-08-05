91854a632b9b329cb0ffc7fc6091a0472eb95bb1
Shadiyar  Alakhan <shadiyar.alakhan@gmail.com>
2022-08-05 19:18:35 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import '@fontsource/roboto/700.css';
+import {Container} from "@mui/material";
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Container fixed>
+          <Box sx={{ flexGrow: 1 }}>
+              <Grid container spacing={2}>
+                  <Grid item xs={8}>
+                      <Item>xs=8</Item>
+                  </Grid>
+                  <Grid item xs={4}>
+                      <Item>xs=4</Item>
+                  </Grid>
+                  <Grid item xs={6}>
+                      <Item>xs=4</Item>
+                  </Grid>
+                  <Grid item xs={4}>
+                      <Item>xs=8</Item>
+                  </Grid>
+                  <Grid item xs={4}>
+                      <Item>xs=8</Item>
+                  </Grid>
+              </Grid>
+          </Box>
+      </Container>
+  )
 }
-
 export default App;
